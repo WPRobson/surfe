a9c9032328806a0935cf8a28ff2fca699d6e5613
Will Robson <wrobson2244@gmail.com>
2025-05-12 20:36:57 +0100

    fix not returning error from user repo

--- internal/repository/userrepository.go
+++ internal/repository/userrepository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"encoding/json"
+	"errors"
 	"os"
 	"surfe/internal/models"
 )
@@ -25,16 +26,23 @@ func (r *userRepository) loadData(filePath string) error {
 	}
 	defer file.Close()
 
-	return json.NewDecoder(file).Decode(&r.users)
+	if err := json.NewDecoder(file).Decode(&r.users); err != nil {
+		return errors.New("invalid JSON data")
+	}
+	return nil
 }
 
 func (r *userRepository) GetByID(id int) (*models.User, error) {
+	if id <= 0 {
+		return nil, errors.New("invalid user ID")
+	}
+
 	for _, user := range r.users {
 		if user.ID == id {
 			return &user, nil
 		}
 	}
-	return nil, nil
+	return nil, errors.New("user not found")
 }
 
 func (r *userRepository) GetAll() ([]models.User, error) {
